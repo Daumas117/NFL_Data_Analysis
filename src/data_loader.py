7204b568aa90681837071b94366cf678fde08183
Jose Daumas <josedaumas@hotmail.com>
2025-10-19 22:33:31 +0200

    Modificacion de la ingesta de informacion y realizacion de validaciones en caso de no cargar la información de manera correcta

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,59 +3,55 @@
 import nflreadpy as nfl
 import polars as pl
 from pathlib import Path
+import datetime
 import argparse
+from datetime import date
 
 # Carpeta principal para datos crudos
 DATA_DIR = Path(__file__).resolve().parents[1] / "data/raw"
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-def download_weekly_data(seasons=[2024], weeks=None):
+# Funcion que realiza la descarga de las temporadas conforme al año indicado.
+def download_weekly_data(season = None):
     """
-    Descarga datos semanales de la NFL y los guarda en formato Parquet.
+    Descarga o actualiza datos de jugadores de la temporada actual.
+    """  
+    if season is None:
+        season = datetime.date.today().year
+    print(f"Descargando información actualizadas para temporada {season}...")
     
-    Args:
-        seasons (list[int]): Lista de temporadas a descargar.
-        weeks (list[int], optional): Lista de semanas a descargar. Si es None, descarga todas.
-    """
-    for season in seasons:
-        print(f"Descargando datos de la temporada {season}...")
+    try:
+        # Descargar datos
+        df = nfl.load_player_stats([season])
+        df_pl = pl.DataFrame(df)
+    except Exception as e:
+        print(f"❌ Error al descargar datos de la temporada{season}: {e}")
+        return  # Sale de la season en caso de un error.
+    # Guardar archivo
+    today = date.today().strftime("%Y%m%d")
+    file_name = f"player_stats_{season}_{today}.parquet"
+    file_path = DATA_DIR / file_name
         
-        try:
-            # Descargar datos
-            df = nfl.load_weekly(years=[season], weeks=weeks)
-            df_pl = pl.DataFrame(df)
-        except Exception as e:
-            print(f"❌ Error al descargar datos: {e}")
-            return # Sale de la función si falla la descarga
-        # Guardar archivo
-        file_name = f"weekly_{season}"
-        if weeks:
-            weeks_str = "_".join(str(w) for w in weeks)
-            file_name += f"_weeks_{weeks_str}"
-        file_name += ".parquet"
-        
-        file_path = DATA_DIR / file_name
+    try:
         df_pl.write_parquet(file_path)
-        print(f"Datos guardados en: {file_path} | Registros: {len(df_pl)}")
+        print(f"Datos actualizados guardados en: {file_path} | Registros: {len(df_pl)}")
+    except Exception as e:
+        print(f"❌ Error al guardar el archivo: {e}")
 
 # -----------------------------
 # Permitir ejecución desde terminal
 # -----------------------------
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Descargar datos semanales de NFL")
+    parser = argparse.ArgumentParser(description="Descargar estadísticas de jugadores de la NFL")
+    
+    # CAMBIO 4: Default en argparse usa el año actual (dinámico)
+    current_year = datetime.date.today().year
     parser.add_argument(
-        "--seasons",
-        type=int,
-        nargs="+",
-        default=[2024],
-        help="Lista de temporadas a descargar, ejemplo: --seasons 2022 2023"
-    )
-    parser.add_argument(
-        "--weeks",
-        type=int,
-        nargs="+",
-        help="Lista de semanas a descargar, ejemplo: --weeks 1 2 3"
+        "--season", 
+        type=int, 
+        default=current_year, # Usamos el año actual como default
+        help=f"Temporada a descargar (default: {current_year})"
     )
     args = parser.parse_args()
-    
-    download_weekly_data(seasons=args.seasons, weeks=args.weeks)
+
+    download_weekly_data(season=args.season)
